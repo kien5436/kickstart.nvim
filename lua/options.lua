661954d78efa80d8c6cf9a91122675a6526ecbd3
Kien <ptkien.tnn@vnpt.vn>
2025-05-21 08:39:48 +0700

    refactor: https://github.com/nvim-lua/kickstart.nvim/pull/1482, https://github.com/nvim-lua/kickstart.nvim/pull/1495, https://github.com/nvim-lua/kickstart.nvim/pull/1516

--- lua/options.lua
+++ lua/options.lua
@@ -1,43 +1,43 @@
 -- fold: https://www.jackfranklin.co.uk/blog/code-folding-in-vim-neovim/
 
-vim.opt.autoindent = true
-vim.opt.breakindent = true
-vim.opt.cursorline = true
-vim.opt.expandtab = true
-vim.opt.foldcolumn = '0'
-vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.opt.foldlevel = 99 -- fold all by default: 1
-vim.opt.foldlevelstart = 0 -- top level folds are opened
-vim.opt.foldmethod = 'expr'
-vim.opt.foldnestmax = 20
-vim.opt.foldtext = ''
-vim.opt.ignorecase = true
-vim.opt.inccommand = 'split'
-vim.opt.list = true
+vim.o.autoindent = true
+vim.o.breakindent = true
+vim.o.cursorline = true
+vim.o.expandtab = true
+vim.o.foldcolumn = '0'
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldlevel = 99 -- fold all by default: 1
+vim.o.foldlevelstart = 0 -- top level folds are opened
+vim.o.foldmethod = 'expr'
+vim.o.foldnestmax = 20
+vim.o.foldtext = ''
+vim.o.ignorecase = true
+vim.o.inccommand = 'split'
+vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.mouse = 'a'
-vim.opt.number = true
-vim.opt.scrolloff = 10
-vim.opt.shiftwidth = 2
-vim.opt.showmode = false
-vim.opt.signcolumn = 'yes'
-vim.opt.smartcase = true
-vim.opt.smarttab = true
-vim.opt.softtabstop = 2
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.tabstop = 2
-vim.opt.timeoutlen = 300
-vim.opt.undofile = true
-vim.opt.updatetime = 250
+vim.o.mouse = 'a'
+vim.o.number = true
+vim.o.scrolloff = 10
+vim.o.shiftwidth = 2
+vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+vim.o.smartcase = true
+vim.o.smarttab = true
+vim.o.softtabstop = 2
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.tabstop = 2
+vim.o.timeoutlen = 300
+vim.o.undofile = true
+vim.o.updatetime = 250
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.opt.confirm = true
+vim.o.confirm = true
 
 vim.schedule(function()
   -- Sync clipboard between OS and Neovim.
-  vim.opt.clipboard = 'unnamedplus'
+  vim.o.clipboard = 'unnamedplus'
 end)
 
 -- Set up the clipboard configuration in Lua
