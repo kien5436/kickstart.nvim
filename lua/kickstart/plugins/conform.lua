7d501595870e087c6e460dd2d1926af05635f852
Kien <ptkien.tnn@vnpt.vn>
2025-07-01 16:17:26 +0700

    feat: format json5 file

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -34,6 +34,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         json = { 'prettierd' },
+        json5 = { 'prettierd' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
