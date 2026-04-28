c04bf8ca99a6641175a9413d7e588c91bcf9d013
Kien <ptkien.tnn@vnpt.vn>
2026-04-28 07:46:03 +0700

    chore: update autopairs config

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -6,7 +6,5 @@ return {
   event = 'InsertEnter',
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
-  config = function()
-    require('nvim-autopairs').setup {}
-  end,
+  config = true,
 }
